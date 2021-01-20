751a9c841ef4084be9754b16d644263a0f9a6b10
André Ramaldes <67028873+Andre07-creator@users.noreply.github.com>
2021-01-20 10:48:07 -0300

    Starting 1.0

---
+++ URI_SortSimples_exer_1042/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace URI_SortSimples_exer_1042
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Value = Console.ReadLine().Split(' ');
+            int v1 = int.Parse(Value[0]);
+            int v2 = int.Parse(Value[1]);
+            int v3 = int.Parse(Value[2]);
+
+            if (v1 < v2 && v1 < v3)
+            {
+                Console.WriteLine(v1);
+            }
+            else if (v2 < v1 && v2 < v3)
+            {
+                Console.WriteLine(v2);
+            }
+            else
+            {
+                Console.WriteLine(v3);
+            }
+
+            if (v1 < v2 && v1 > v3)
+            {
+                Console.WriteLine(v1);
+            }
+            else if (v2 < v1 && v2 > v3)
+            {
+                Console.WriteLine(v2);
+            }
+            else
+            {
+                Console.WriteLine(v3);
+            }
+            if (v1 > v2 && v1 > v3)
+            {
+                Console.WriteLine(v1);
+            }
+            else if (v2 > v3 && v2 > v3)
+            {
+                Console.WriteLine(v2);
+            }
+            else
+            {
+                Console.WriteLine(v3);
+            }
+            Console.WriteLine();
+            Console.WriteLine(v1);
+            Console.WriteLine(v2);
+            Console.WriteLine(v3);
+
+        }
+    }
+}
