0b89c3e42ec513f64644909a3b5c788fd7faf0f9
André Ramaldes <67028873+Andre07-creator@users.noreply.github.com>
2021-01-20 21:01:15 -0300

    Update V1.1

--- URI_SortSimples_exer_1042/Program.cs
+++ URI_SortSimples_exer_1042/Program.cs
@@ -1,58 +1,31 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace URI_SortSimples_exer_1042
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<int> list = new List<int>();
             string[] Value = Console.ReadLine().Split(' ');
             int v1 = int.Parse(Value[0]);
             int v2 = int.Parse(Value[1]);
             int v3 = int.Parse(Value[2]);
+            list.Add(v1);
+            list.Add(v2);
+            list.Add(v3);
 
-            if (v1 < v2 && v1 < v3)
+            list.Sort();
+            foreach (int s in list)
             {
-                Console.WriteLine(v1);
-            }
-            else if (v2 < v1 && v2 < v3)
-            {
-                Console.WriteLine(v2);
-            }
-            else
-            {
-                Console.WriteLine(v3);
+                Console.WriteLine(s);
             }
 
-            if (v1 < v2 && v1 > v3)
-            {
-                Console.WriteLine(v1);
-            }
-            else if (v2 < v1 && v2 > v3)
-            {
-                Console.WriteLine(v2);
-            }
-            else
-            {
-                Console.WriteLine(v3);
-            }
-            if (v1 > v2 && v1 > v3)
-            {
-                Console.WriteLine(v1);
-            }
-            else if (v2 > v3 && v2 > v3)
-            {
-                Console.WriteLine(v2);
-            }
-            else
-            {
-                Console.WriteLine(v3);
-            }
             Console.WriteLine();
             Console.WriteLine(v1);
             Console.WriteLine(v2);
             Console.WriteLine(v3);
-
         }
+
     }
 }
